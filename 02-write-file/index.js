8588e2848183a3ac95bb5db99d944d512ff59a55
CerBeer <ocelopp@gmail.com>
2024-01-20 15:42:37 +0300

    fix: remove flag append when open file to write in 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,15 +3,14 @@ const fs = require('node:fs');
 const { stdin, stdout } = require('node:process');
 
 const fileFromConsole = path.join('./02-write-file', 'text.txt');
-const fout = fs.createWriteStream(fileFromConsole, { flags: 'a' });
+const fout = fs.createWriteStream(fileFromConsole);
 
 stdout.write('Hello! Enter text below:\n');
 stdin.on('data', (data) => {
   if (data.toString().trim().toLowerCase() === 'exit') process.exit();
   fout.write(data);
 });
-// fs.createReadStream(fileFromConsole).pipe(stdout);
 
 process.on('SIGINT', () => process.exit());
 
-process.on('exit', () => stdout.write('Goodbye! Have a nice day\n'));
+process.on('exit', () => stdout.write('Goodbye! Have a nice day!\n'));
